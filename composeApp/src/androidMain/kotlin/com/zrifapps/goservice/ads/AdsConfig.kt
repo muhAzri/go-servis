61d550ffb770931ae05f4d626f6a48f9b6f1273c
Azri <muhammad.azri.f.s@gmail.com>
2026-06-02 19:24:19 +0700

    chore(ads): set production Yandex ad unit IDs (Android)
    Replace the demo Yandex units with the real production unit IDs for all
    four formats: banner, interstitial, app open, and native.

--- composeApp/src/androidMain/kotlin/com/zrifapps/goservice/ads/AdsConfig.kt
+++ composeApp/src/androidMain/kotlin/com/zrifapps/goservice/ads/AdsConfig.kt
@@ -1,8 +1,8 @@
 package com.zrifapps.goservice.ads
 
 object AdsConfig {
-    const val BANNER_UNIT_ID = "demo-banner-yandex"
-    const val INTERSTITIAL_UNIT_ID = "demo-interstitial-yandex"
-    const val NATIVE_CONTENT_UNIT_ID = "demo-native-content-yandex"
-    const val APP_OPEN_UNIT_ID = "demo-appopenad-yandex"
+    const val BANNER_UNIT_ID = "R-M-19246009-1"
+    const val INTERSTITIAL_UNIT_ID = "R-M-19246009-2"
+    const val NATIVE_CONTENT_UNIT_ID = "R-M-19246009-4"
+    const val APP_OPEN_UNIT_ID = "R-M-19246009-3"
 }
